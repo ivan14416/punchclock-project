07a97d617951e567355245e15f5274f557346bd2
Ivan <starcevic-ivan@outlook.com>
2020-09-24 16:12:25 +0200

    README.md added

---
+++ README.md
@@ -0,0 +1,14 @@
+-Backend starten:
+	1. Ordner "m223-punchclock-master" in IntelliJ öffnen
+	2. Programm starten
+
+-Frontend starten:
+	1. xampp herunterladen https://www.apachefriends.org/de/index.html
+	2. Ordner "punchclock-ui" im Ordner "xampp/htdocs/" verschieben
+	3. Im controll Fenster von xampp Apache starten
+	4. im Browser "http://localhost/punchclock-ui/login.php" öffnen
+	5. einloggen mit username: test, password: test
+
+
+Im Programm kann man sich monentan einloggen, alle Zeiteinträge auslesen lassen und sie auch löschen.
+Das erstellen von Zeiteinträge funktioniert momentan nicht.
--- m223-punchclock-master/src/main/resources/data.sql
+++ m223-punchclock-master/src/main/resources/data.sql
@@ -1,5 +1,4 @@
-INSERT INTO APPLICATION_USER (id, password, username) VALUES (1, '2a$10$i4INelEJBbELiXzBCKc6iOff9ibgZf2AP5IS.15sV5yvD2ewDEB0W', 'test');
+INSERT INTO APPLICATION_USER (id, password, username) VALUES (1, '$2a$10$8ZEONd7h4yM987w36L9Im.1lgIqqP/30lY/zSRBCXg4nLkVyJbhTm', 'test');
 
 INSERT INTO ENTRY (id, check_in, check_out, applicationuser_id) VALUES (1, '2020-09-30T03:40:20.345Z', '2020-10-24T11:35:21.345Z', 1);
-INSERT INTO ENTRY (id, check_in, check_out, applicationuser_id) VALUES (2, '2020-09-30T15:06:08.345Z', '2020-09-24T11:40:20.345Z', 2);
-INSERT INTO ENTRY (id, check_in, check_out, applicationuser_id) VALUES (3, '2020-09-30T12:43:21.345Z', '2020-09-24T11:50:21.345Z', 2)
+INSERT INTO ENTRY (id, check_in, check_out, applicationuser_id) VALUES (2, '2020-09-30T15:06:08.345Z', '2020-09-24T11:40:20.345Z', 1);
